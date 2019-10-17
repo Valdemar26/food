f667832f46074ce5ca1ebac275646f7ada1dd1b0
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-10-17 15:51:18 +0300

    make some architecture

--- src/app/shared/delivery/delivery.module.ts
+++ src/app/shared/delivery/delivery.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header.component';
+import { DeliveryComponent } from './delivery.component';
 import {RouterModule, Routes} from '@angular/router';
 import {RestaurantComponent} from '../restaurant/restaurant.component';
 
 const routes: Routes = [
-  {path: '',  component: HeaderComponent}
+  {path: '',  component: DeliveryComponent}
 ];
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    DeliveryComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
-  ],
-  exports: [
-    HeaderComponent
   ]
 })
-export class HeaderModule { }
+export class DeliveryModule { }
--- src/app/shared/main/main.module.ts
+++ src/app/shared/main/main.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header.component';
+import { MainComponent } from './main.component';
 import {RouterModule, Routes} from '@angular/router';
-import {RestaurantComponent} from '../restaurant/restaurant.component';
 
 const routes: Routes = [
-  {path: '',  component: HeaderComponent}
+  {path: '',  component: MainComponent}
 ];
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [MainComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
   ],
   exports: [
-    HeaderComponent
+    MainComponent
   ]
 })
-export class HeaderModule { }
+export class MainModule { }
