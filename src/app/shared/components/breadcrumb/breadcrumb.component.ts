27a877c2c4e0307a2b9c3b0a550df99d0d1709a1
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-11-05 12:36:44 +0200

    refactor routing, working with breadcrumbs

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {DishesService} from '../../services/dishes.service';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -9,14 +10,27 @@ import {ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, NavigationStart, 
 export class BreadcrumbComponent implements OnInit {
 
   location: any;
+  breadCrumb;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private dishService: DishesService
     ) { }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log(event.url);
+        this.breadCrumb = event.url.split('/').slice(1);
+      }
+    });
+  }
 
+  navigateToRoute(route) {
+    // console.log('route: ', route, route.split('/')[2] ? route.split('/')[2] : null);
+
+    // this.dishService.getDishByType()
   }
 
 }
