8e4088f723eca27424eb5d169786a934ad22620b
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-11-14 16:44:32 +0200

    added agm-module

--- src/app/shared/contacts/contacts.component.ts
+++ src/app/shared/contacts/contacts.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  lat = 49.848485;
+  lng = 24.020991;
 
+  private points = [
+    {
+      lat: 49.948485,
+      lng: 24.120991
+    },
+    {
+      lat: 49.748487,
+      lng: 24.220993
+    },
+    {
+      lat: 49.648483,
+      lng: 24.420998
+    }
+  ];
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
